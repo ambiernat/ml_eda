3c9b8997f8b6ae0ba121188116218c2f474b04ee
ambiernat <43310781+ambiernat@users.noreply.github.com>
2025-03-08 17:17:57 +0000

    Update column_types.py

--- column_types.py
+++ column_types.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import sys
 
+
+mode = input("Paste path to the training data and press enter (without the '' marks), e.g. /content/drive/MyDrive/Kaggle/kaggle_data/train.csv: ").strip()
+data = pd.read_csv(mode)
+
 # identifying columns which have different values and so are useless for prediction:
 def identifier_cols_fctn(df):
   colnames_return=[]
@@ -11,30 +15,30 @@ def identifier_cols_fctn(df):
     else: pass
     return colnames_return
 
-identifier_cols = identifier_cols_fctn(train_data)
+identifier_cols = identifier_cols_fctn(data)
 
-numerical_cols=list(train_data.select_dtypes(include='number'))
+numerical_cols=list(data.select_dtypes(include='number'))
 
-bool_cols=list(train_data.select_dtypes(include='bool'))
+bool_cols=list(data.select_dtypes(include='bool'))
 
-object_cols=list(train_data.select_dtypes(include='object'))
+object_cols=list(data.select_dtypes(include='object'))
 
-category_cols=list(train_data.select_dtypes(include='category'))
+category_cols=list(data.select_dtypes(include='category'))
 
-datetime_cols=list(train_data.select_dtypes(include='datetime'))
+datetime_cols=list(data.select_dtypes(include='datetime'))
 
 # identifying columns containing missing values
 def missing_val_cols_fctn(df):
   colnames_return=[]
-  for col in train_data.columns.tolist():
-    if sum(train_data[col].isnull()) != 0:
+  for col in data.columns.tolist():
+    if sum(data[col].isnull()) != 0:
       colnames_return.append(col)
     else: pass
     return colnames_return
 
-missing_val_cols=missing_val_cols_fctn(train_data)
+missing_val_cols=missing_val_cols_fctn(data)
 
-other = list(set(train_data.columns.tolist()).difference(set(identifier_cols+numerical_cols+bool_cols+object_cols+category_cols+datetime_cols+missing_val_cols)))
+other = list(set(data.columns.tolist()).difference(set(identifier_cols+numerical_cols+bool_cols+object_cols+category_cols+datetime_cols+missing_val_cols)))
 
 eda_vals=[str(identifier_cols), str(numerical_cols), str(bool_cols), str(object_cols), str(category_cols), str(datetime_cols), str(missing_val_cols), str(other)]
 
@@ -45,6 +49,9 @@ eda_col_types = dict(zip(eda_keys, eda_vals))
 col_types_eda_df = pd.DataFrame.from_dict(eda_col_types, orient='index').rename(columns={0:'column_names'})
 
 with pd.option_context('display.max_colwidth', None):
-  display(col_types_eda_df)
+  col_types_eda_df
 
-del eda_col_types, eda_keys, eda_keys, eda_vals, other, missing_val_cols, datetime_cols, category_cols, object_cols, bool_cols, numerical_cols, identifier_cols
+del eda_col_types, eda_keys, eda_vals, other, missing_val_cols, datetime_cols, category_cols, object_cols, bool_cols, numerical_cols, identifier_cols
+
+print(col_types_eda_df)
+print('Dataframe Name:col_types_eda_df')
